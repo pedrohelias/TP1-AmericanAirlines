1a74b7acc9a4ec67eb39dd4878764e10f268b4d3
Pedro Paulo Monte Pagani <56891617+PedroMPagani@users.noreply.github.com>
2023-01-12 10:43:34 -0300

    Add add sample method
    This allows us to see the actual number of delayed flights etc per flight lengths.

--- code.c
+++ code.c
@@ -3,12 +3,12 @@
 #include <string.h>
 
 // Estrutura para armazenar os dados de cada linha do arquivo CSV
-typedef struct  {
+typedef struct {
   int id;
   int Time;
   int Length;
   int Delay;
-}Data;
+} Data;
 
 typedef struct {
   int length;
@@ -21,11 +21,7 @@ int main(void) {
 int dataSize = 1;
 int dataCount = 0;
 Data *data = NULL;
-
   char line[1024];
-
-
-
 
   // Abre o arquivo para leitura
   FILE *fp = fopen("bancoAjustado4colunas.csv", "r");
@@ -35,12 +31,8 @@ Data *data = NULL;
     perror("Error opening file");
     return 1;
   }
-
   // Aloca memória para a lista de estruturas
-
-
   // Lê o arquivo linha por linha e armazena os dados em uma estrutura
-
     while (fgets(line, 1024, fp)) {  
        Data *new;
         new = realloc(data,dataSize * sizeof *data);
@@ -71,8 +63,13 @@ Data *data = NULL;
   int trial = 0;
   int minLength = 99999;
   int maxLength = -99999;
+  Subspace* spaces = NULL;
+  
   for (int j = 1; j < (dataCount - 1)/10; j++){
     Data key = data[j];
+    
+    addSample(spaces, key.Length, key.Delay);
+    
     if(key.Length > maxLength){
       maxLength = key.Length;
     }
@@ -87,22 +84,48 @@ Data *data = NULL;
     }
     data[k + 1] = key;
   }
-
-  
   printf("maxLength: %d minLength %d\n", maxLength, minLength);
-  
-
-
   // Imprime as estruturas ordenadas
   for (int j = 0; j < (dataCount - 1)/10; j++) {
     fprintf(fp_out, "%d,%d,%d,%d\n", data[j].id, data[j].Time, data[j].Length, data[j].Delay);
   }
-
+  
+  int index = (sizeof(spaces)/sizeof(Subspace));
+  for(int k = 0; k < index; k++){
+    Subspace spc = spaces[k];
+    printf("length: %d, total delayed: %d, total flights %d", spc.length, spc.delayed, spc.total);
+  }
+  
  // Fecha o arquivo de saída
   fclose(fp_out);
-
   // Libera a memória alocada para a lista
   free(data);
-
   return 0;
+}
+
+void addSample(Subspace* spaces, int length, int delay){
+  int indexes = 0;
+  if(spaces != NULL){
+     indexes = (sizeof(spaces)/sizeof(Subspace));
+  }
+  for(int i = 0; i < indexes; i++){
+    Subspace subp = spaces[i];
+    if(subp.length == length){
+       subp.total++;
+       if(delay == 1){
+          subp.delayed++;
+       }
+       return; // para para nao criar mais espaco em memoria
+    }
+  }
+  Subspace* spc = realloc(spaces ,(indexes + 1) * sizeof(Subspace));
+  Subspace subp = spc[indexes];
+  subp.length = length;
+  subp.total++;
+  if(delay == 1){
+    subp.delayed++;
+  }
+  spaces = spc;
+}
+  
 }
